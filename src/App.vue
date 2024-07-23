dc87ab5097b44e0ad161f8b58970fe2af2421a15
TatianaChF <tatiana.chefox.gmail.com>
2024-07-23 20:02:32 +0300

    placing information in the center

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Давайте разделим счет</h1>
-    <a>Начать</a>
+    <a href="#">Начать</a>
   </div>
 </template>
+
+<style scoped>
+  .container {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+  }
+</style>
 
