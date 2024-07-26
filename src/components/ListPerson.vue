6cf302a4acaa7c41fe86c55671369b4a5874f32e
TatianaChF <tatiana.chefox.gmail.com>
2024-07-26 09:06:04 +0300

    adding button component

--- src/components/ListPerson.vue
+++ src/components/ListPerson.vue
@@ -1,14 +1,17 @@
 <template>
-    <div class="container container__form-person">
-        <v-btn variant="tonal">Добавить человека</v-btn>
-        <v-list lines="one">
-            <v-list-item
-                v-for="n in 6"
-                :key="n"
-                :title="'Item ' + n"
-                subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"
-            ></v-list-item>
-        </v-list>
+    <div class="container">
+        <div class="container__form-person">
+            <v-btn variant="tonal">Добавить человека</v-btn>
+            <v-list lines="one">
+                <v-list-item
+                    v-for="n in 6"
+                    :key="n"
+                    :title="'Item ' + n"
+                    subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+                ></v-list-item>
+            </v-list>
+        </div>
+        <v-btn variant="tonal">Дальше!</v-btn>
     </div>
 </template>
 
