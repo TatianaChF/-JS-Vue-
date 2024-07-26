52b1fcb26d7a661f74042b6f9698d803ca971be1
TatianaChF <tatiana.chefox.gmail.com>
2024-07-26 10:21:26 +0300

    creating a form

---
+++ src/components/FormPerson.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-sheet class="mx-auto" width="300">
+        <v-form class="form" @submit.prevent>
+            <v-text-field
+                v-model="firstName"
+                :rules="rules"
+                label="Имя"
+            ></v-text-field>
+            <v-btn class="mt-2" type="submit" variant="tonal" block>Добавить</v-btn>
+        </v-form>
+    </v-sheet>
+</template>
+
+<style lang="scss" scoped>
+    .form {
+        padding: 20px 30px;
+        border: 1px solid grey;
+    }
+</style>
