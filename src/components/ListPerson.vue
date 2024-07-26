f02ab9f525b0243d7ef5f7168a4d5fdabbca84b8
TatianaChF <tatiana.chefox.gmail.com>
2024-07-26 09:01:19 +0300

    adding a list component and a button

--- src/components/ListPerson.vue
+++ src/components/ListPerson.vue
@@ -1,3 +1,17 @@
 <template>
-    <h1>Test</h1>
+    <div class="container container__form-person">
+        <v-btn variant="tonal">Добавить человека</v-btn>
+        <v-list lines="one">
+            <v-list-item
+                v-for="n in 6"
+                :key="n"
+                :title="'Item ' + n"
+                subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+            ></v-list-item>
+        </v-list>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+    @import './../assets/style.scss'
+</style>
