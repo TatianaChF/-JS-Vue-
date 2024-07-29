dd5a954d0844f9f9bd1862313f62f60ae4fbd247
tikaya <tatiana.chefox@gmail.com>
2024-07-29 11:51:09 +0300

    add tracking of the input of the name

--- src/components/FormPerson.vue
+++ src/components/FormPerson.vue
@@ -1,31 +1,34 @@
 <template>
-    <div class="container">
-        <v-sheet class="mx-auto" width="300">
-            <v-form class="form" @submit.prevent>
-                <v-text-field
-                    v-model="firstName"
-                    :rules="rules"
-                    label="Имя"
-                ></v-text-field>
-                <v-btn class="mt-2" type="submit" variant="tonal" block>Добавить</v-btn>
-            </v-form>
-        </v-sheet>
-        <v-btn variant="text" @click="$emit('changeOpen')">x</v-btn>
-    </div>
+  <div class="container">
+    <v-sheet class="mx-auto" width="300">
+      <v-form class="form" @submit.prevent>
+        <v-text-field
+          v-model="firstName"
+          :rules="rules"
+          label="Имя"
+        ></v-text-field>
+        <v-btn class="mt-2" type="submit" variant="tonal" @click="$emit('addPerson', firstName)" block>Добавить</v-btn>
+      </v-form>
+    </v-sheet>
+    <v-btn variant="text" @click="$emit('changeOpen')">x</v-btn>
+  </div>
 </template>
 
 <script setup>
-    const props = defineProps(['open']);
+import { ref } from 'vue';
+
+const props = defineProps(["open"]);
+let firstName = ref("");
 </script>
 
 <style lang="scss" scoped>
-    .form {
-        padding: 20px 30px;
-        border: 1px solid grey;
-    }
+.form {
+  padding: 20px 30px;
+  border: 1px solid grey;
+}
 
-    .container {
-        display: flex;
-        flex-direction: row;
-    }
+.container {
+  display: flex;
+  flex-direction: row;
+}
 </style>
