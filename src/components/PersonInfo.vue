80a7bbdbc5527f0eec72eece483fe2d2b14f464d
tikaya <tatiana.chefox@gmail.com>
2024-07-29 11:02:24 +0300

    Adding buttons to a separate div

--- src/components/PersonInfo.vue
+++ src/components/PersonInfo.vue
@@ -1,13 +1,15 @@
 <template>
-    <p>{{ name }}</p>
+  <p>{{ name }}</p>
+  <div>
     <v-btn>
-        <v-icon icon="mdi-border-color"></v-icon>
+      <v-icon icon="mdi-border-color"></v-icon>
     </v-btn>
     <v-btn>
-        <v-icon icon="mdi-delete"></v-icon>
+      <v-icon icon="mdi-delete"></v-icon>
     </v-btn>
+  </div>
 </template>
 
 <script setup>
-const props = defineProps(['name']);
+const props = defineProps(["name"]);
 </script>
