e2cd72178e0500da0712fad22211b6bd59e66a65
tikaya <tatiana.chefox@gmail.com>
2024-07-31 18:12:50 +0300

    getting a list of people from the store

--- src/components/ListPerson.vue
+++ src/components/ListPerson.vue
@@ -2,11 +2,11 @@
   <div :class="changeStyles">
     <div class="container__form-person">
       <v-btn @click="isOpen = true" variant="tonal">Добавить человека</v-btn>
-      <v-card max-width="900" v-if="persons.length > 0">
+      <v-card max-width="900" v-if="3 > 0">
         <v-list lines="three">
           <v-list-item
-            v-for="person in persons"
-            :key="person.id"
+            v-for="person in personsStore.persons"
+            :key="person"
             :title="name"
           >
             <person-info 
@@ -24,7 +24,6 @@
   <FormPerson
     v-if="isOpen"
     :open="isOpen"
-    :id="persons.length"
     @change-open="isOpen = false"
     @add-person="(person) => {
         persons.push(person);
@@ -44,12 +43,7 @@ import PersonInfo from "./PersonInfo.vue";
 import { usePersonsStore } from "./../store/persons";
 
 let isOpen = ref(false);
-let persons = ref([
-    {id: 1, firstName: 'A'},
-    {id: 2, firstName: 'B'},
-    {id: 3, firstName: 'C'}
-]);
-const personStore = usePersonsStore();
+const personsStore = usePersonsStore();
 
 defineProps({
   open: Boolean,
