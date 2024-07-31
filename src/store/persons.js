e37e9cb6f7edabd802bd0037037fda2ce9e15d8e
tikaya <tatiana.chefox@gmail.com>
2024-07-31 18:14:58 +0300

    adding persons state

--- src/store/persons.js
+++ src/store/persons.js
@@ -1,8 +1,14 @@
 import { defineStore } from 'pinia'
 import { computed, ref } from 'vue';
 
-export const usePersonsStore = defineStore('persons', () => {
-    let persons = ref(['A']);
+export const usePersonsStore = defineStore('persons', {
+    state: () => ({
+        persons: ['A', 'B', 'C']
+    })
+
+})
+
+/* let persons = ref(['A', 'B', 'C']);
 
     function addPersonName(name) {
         persons.push(name);
@@ -10,6 +16,4 @@ export const usePersonsStore = defineStore('persons', () => {
 
     function deletePerson(name) {
         persons = persons.filter(person => person !== name);
-    }
-
-})
+    } */
