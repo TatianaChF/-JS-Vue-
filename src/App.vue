bf4c27b02ec4c203de34c59f975471f54990d3a9
tikaya <tatiana.chefox@gmail.com>
2024-08-01 11:40:51 +0300

    delete change theme

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <template>
-  <v-app>
-    <v-btn @click="toggleTheme">Изменить цвет темы</v-btn>
-  </v-app>
   <RouterView />
 </template>
 
 <script setup>
   import { RouterView } from 'vue-router';
-  import { useTheme } from 'vuetify'
-  
-  const theme = useTheme()
-  
-  function toggleTheme () {
-    theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
-  }
 </script>
