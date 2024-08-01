52d8c1f6ea1a89db013e09a4df027f2057daff81
tikaya <tatiana.chefox@gmail.com>
2024-08-01 09:37:28 +0300

    creating a test object of dishes and displaying the names on the screen

--- src/components/DishesPage.vue
+++ src/components/DishesPage.vue
@@ -1,17 +1,10 @@
 <template>
   <v-container class="container container__form">
-    <v-btn
-      text="Добавить блюдо"
-    />
-    <dish-card
-    />
+    <v-btn text="Добавить блюдо" />
+    <p v-for="dish in dishes">{{ dish.name }}</p>
     <div class="container__form__btns">
-      <v-btn
-        text="Назад"
-      />
-      <v-btn
-        text="Рассчитать"
-      />
+      <v-btn text="Назад" />
+      <v-btn text="Рассчитать" />
     </div>
     <v-card class="h4-dish-page">
       <h4>Итог:</h4>
@@ -20,7 +13,30 @@
   </v-container>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+let dishes = ref([
+  {
+    id: 1,
+    name: "Блюдо 1",
+    payer: "A",
+    whoEat: ["A", "B"],
+  },
+  {
+    id: 2,
+    name: "Блюдо 2",
+    payer: "A",
+    whoEat: ["B", "C"],
+  },
+  {
+    id: 3,
+    name: "Блюдо 3",
+    payer: "A",
+    whoEat: ["A", "C"],
+  }
+]);
+</script>
 
 <style lang="scss" scoped>
 @import "./../assets/style.scss";
