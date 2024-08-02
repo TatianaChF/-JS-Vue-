69557c8530c235dedb33754b5d63c496ff8cd302
TatianaChF <tatiana.chefox.gmail.com>
2024-08-02 12:10:40 +0300

    adding actions

--- src/store/dishes.js
+++ src/store/dishes.js
@@ -1,29 +1,36 @@
-import { defineStore } from "pinia";
+import { defineStore } from 'pinia'
 
 export const useDishesStore = defineStore('dishes', {
     state: () => ({
-        dishes: [
-            {
-              id: 1,
-              name: "Блюдо 1",
-              payer: "A",
-              whoEat: ["A", "B"],
-              price: 100,
-            },
-            {
-              id: 2,
-              name: "Блюдо 2",
-              payer: "A",
-              whoEat: ["B", "C"],
-              price: 30,
-            },
-            {
-              id: 3,
-              name: "Блюдо 3",
-              payer: "A",
-              whoEat: ["A", "C"],
-              price: 500,
-            },
-        ]
-    })
+        dishes: [{
+            id: 1,
+            name: "Блюдо 1",
+            payer: "A",
+            whoEat: ["A", "B"],
+            price: 100
+        }, 
+        {
+            id: 2,
+            name: "Блюдо 2",
+            payer: "A",
+            whoEat: ["B", "C"],
+            price: 30
+        }, {
+            id: 3,
+            name: "Блюдо 3",
+            payer: "A",
+            whoEat: ["A", "C"],
+            price: 500
+        }]
+    }),
+    actions: {
+        addDish(dish) {
+            this.dishes.push(dish);
+        },
+
+        removeDish(dishName) {
+            this.dishes = this.dishes.filter(dish => dish.name !== dishName);
+        }
+    }
+
 })
