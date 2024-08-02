b16a4eba7463be1fc0653a9ae8c3730bbbdbccd3
TatianaChF <tatiana.chefox.gmail.com>
2024-08-02 08:56:39 +0300

    adding creation of new dish object

--- src/components/FormDishes.vue
+++ src/components/FormDishes.vue
@@ -2,23 +2,24 @@
   <div class="container">
     <v-sheet class="mx-auto" width="500">
       <v-form class="form" @submit.prevent>
-        <v-text-field v-model="person" :rules="rules" label="Название блюда" />
-        <v-text-field v-model="person" :rules="rules" label="Цена" />
+        <v-text-field v-model="dish.name" :rules="rules" label="Название блюда" />
+        <v-text-field v-model="dish.price" :rules="rules" label="Цена" />
         <v-select
           :items="personsStore.persons.map((person) => person)"
           label="Кто платил?"
+          v-model="dish.payer"
         />
-        <v-btn-toggle>
+        <v-btn-toggle v-model="toggleMultiple" multiple>
           <p>Кто ел?</p>
           <div v-for="(person, index) in personsStore.persons" :key="index">
-            <v-btn>{{ person }}</v-btn>
+            <v-btn @click="onClickAddWhoEat">{{ person }}</v-btn>
           </div>
         </v-btn-toggle>
         <v-btn
           class="mt-2"
           type="submit"
           variant="tonal"
-          @click="$emit('addPerson', person)"
+          @click="console.log(dish)"
           block
           >Добавить</v-btn
         >
@@ -40,6 +41,17 @@ let dish = ref({
   whoEat: [],
   price: 0
 })
+let toggleMultiple = ref([]);
+
+const onClickAddWhoEat = () => {
+  console.log(toggleMultiple.value);
+  for (let i = 0; i < toggleMultiple.value.length; i++ ) {
+    if (!dish.value.whoEat.includes(personsStore.persons[toggleMultiple.value[i]])) {
+      dish.value.whoEat.push(personsStore.persons[toggleMultiple.value[i]]);
+    }
+  }
+  console.log(dish.value.whoEat);
+}
 </script>
 
 <style lang="scss" scoped>
