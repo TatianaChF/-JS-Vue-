459115361de1c07e59f74bbc4e325c6c485fd8c2
tikaya <tatiana.chefox@gmail.com>
2024-08-05 10:01:53 +0300

    Creating cards and buttons on the results page

--- src/components/ResultsPage.vue
+++ src/components/ResultsPage.vue
@@ -1,7 +1,19 @@
 <template>
+  <v-container class="container">
     <v-card title="Результаты">
-
+        <p>Пользователь A кому-то должен?</p>
+        <p>Пользователь В кому-то должен?</p>
+        <p>Пользователь С кому-то должен?</p>
     </v-card>
+    <div class="container__form__btns">
+      <v-btn text="Назад"></v-btn>
+      <v-btn text="На главную"></v-btn>
+    </div>
+  </v-container>
 </template>
 
 <script setup></script>
+
+<style lang="scss" scoped>
+@import "./../assets/style.scss";
+</style>
