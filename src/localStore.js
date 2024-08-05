77d271ea5d233cc5fc589c28b818169a57085543
tikaya <tatiana.chefox@gmail.com>
2024-08-05 13:05:51 +0300

    adding the function of loading data from local storage and exporting functions

--- src/localStore.js
+++ src/localStore.js
@@ -1,3 +1,10 @@
 const saveToLocalStorage = (key, items) => {
     localStorage.getItem(key, JSON.stringify(items));
 }
+
+const loadFromLocalStorage = (key) => {
+    const items = localStorage.getItem(key);
+    return items ? JSON.parse(items) : null; 
+}
+
+export { saveToLocalStorage, loadFromLocalStorage }
