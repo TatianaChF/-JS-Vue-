5e12eea24842047de12937e2a1c6e6c4712417bf
tikaya <tatiana.chefox@gmail.com>
2024-08-05 11:54:37 +0300

    create a loop to calculate the cost for each user

--- src/components/ResultsPage.vue
+++ src/components/ResultsPage.vue
@@ -14,8 +14,30 @@
 
 <script setup>
 import { useRouter } from 'vue-router';
+import { usePersonsStore } from '../store/persons';
+import { useDishesStore } from '../store/dishes';
+import { ref } from 'vue';
 
 const router = useRouter();
+const personsStore = usePersonsStore();
+const dishesStore = useDishesStore();
+const whoPay = ref([]);
+
+for (let i = 0; i < personsStore.persons.length; i++) {
+    let currentPerson = personsStore.persons[i];
+    let total = 0;
+    for (let j = 0; j < dishesStore.dishes.length; j++) {
+        let currentDish = dishesStore.dishes[j];
+        let prcieForOne = currentDish.price / currentDish.whoEat.length;
+
+        if (currentDish.whoEat.includes(currentPerson)) {
+            total += prcieForOne;
+        }
+    }
+
+    console.log(`${currentPerson} должен ${total} рублей`)
+}
+
 </script>
 
 <style lang="scss" scoped>
