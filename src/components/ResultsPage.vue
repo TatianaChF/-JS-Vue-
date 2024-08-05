5d63bfedc06427fe7ff42888cf6a9d00496b4d83
tikaya <tatiana.chefox@gmail.com>
2024-08-05 10:09:46 +0300

    Adding transitions to other pages to the pages of the results

--- src/components/ResultsPage.vue
+++ src/components/ResultsPage.vue
@@ -6,13 +6,17 @@
         <p>Пользователь С кому-то должен?</p>
     </v-card>
     <div class="container__form__btns">
-      <v-btn text="Назад"></v-btn>
-      <v-btn text="На главную"></v-btn>
+      <v-btn @click="router.push({name: 'Dishes'})" text="Назад"></v-btn>
+      <v-btn @click="router.push({name: 'Home'})" text="На главную"></v-btn>
     </div>
   </v-container>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
 
 <style lang="scss" scoped>
 @import "./../assets/style.scss";
