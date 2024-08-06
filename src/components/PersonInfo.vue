00f2811ea4f6612ba1fee30b7865f06a3fc451ee
TatianaChF <tatiana.chefox.gmail.com>
2024-08-06 20:06:46 +0300

    replacing name with id

--- src/components/PersonInfo.vue
+++ src/components/PersonInfo.vue
@@ -1,7 +1,7 @@
 <template>
   <p>{{ props.person.name }}</p>
   <div>
-    <v-btn @click="$emit('removePerson', props.person )">
+    <v-btn @click="$emit('removePerson', props.person.id )">
       <v-icon icon="mdi-delete"></v-icon>
     </v-btn>
   </div>
