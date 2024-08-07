0e776d3bc4f6ecfb685a156e4c63b67b171cdd87
tikaya <tatiana.chefox@gmail.com>
2024-08-07 11:53:35 +0300

    delete JSON.parse

--- src/store/persons.js
+++ src/store/persons.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const usePersonsStore = defineStore('persons', {
     state: () => {
-        const personsData = JSON.parse(localStorage.getItem('personsData'))._value;
+        const personsData = localStorage.getItem('personsData');
         return {
             persons: personsData || []
         }
