333188c3d6d23eb5af7538b38efe852e00ff28da
tikaya <tatiana.chefox@gmail.com>
2024-08-08 13:57:47 +0300

    adding local storage to store and adding watch

--- src/store/dishes.js
+++ src/store/dishes.js
@@ -1,30 +1,44 @@
 import { defineStore } from 'pinia'
+import { computed, ref, watch } from 'vue'
 
-export const useDishesStore = defineStore('dishes', {
-    state: () => {
-        const dishesData = localStorage.getItem('dishesData');
-        return {
-            dishes: dishesData || []
-        }
-    },
-    getters: {
-        totalPrice: (state) => {
-            if(state.dishes.length == 0) return 0
-            else {
-                for(let i = 0; i < state.dishes.length; i++) {
-                    return JSON.parse(state.dishes)._value.reduce((acc, dish) => acc + parseFloat(dish.price), 0);
-                }
-            }
-        }
-    },
-    actions: {
-        addDish(dish) {
-            this.dishes.push(dish);
-        },
+export const useDishesStore = defineStore('dishesData', () => {
+    const dishes = ref([]);
 
-        removeDish(dishName) {
-            this.dishes = this.dishes.filter(dish => dish.name !== dishName);
-        }
+    const dishesLocalStorage = localStorage.getItem("dishesData");
+
+    if (dishesLocalStorage) {
+        dishes.value = JSON.parse(dishesLocalStorage)._value;
     }
 
+    const totalPrice = computed(() => {
+        if(dishes.value.length == 0) return 0
+        else {
+            for (let i = 0; i < dishes.value.length; i++) {
+                return dishes.value.reduce((acc, dish) => acc + parseFloat(dish.price), 0);
+            }
+        }
+    })
+
+    const addDish = (dish) => {
+        dishes.value.push(dish);
+    }
+
+    const removeDish = (dishName) => {
+        dishes.value = dishes.value.filter(dish => dish.name !== dishName) 
+    }
+
+    watch(
+        () => dishes,
+        (store) => {
+        localStorage.setItem("dishesData", JSON.stringify(store));
+        },
+        { deep: true }
+    );
+
+    return {
+        dishes,
+        addDish,
+        removeDish,
+        totalPrice
+    }
 })
