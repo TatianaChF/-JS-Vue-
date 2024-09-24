a40fdb3233bca11d6381de490db66e1d38983f40
TatianaChF <tatiana.chefox.gmail.com>
2024-09-24 18:48:46 +0300

    moving vuetify connection to a separate file

--- src/main.js
+++ src/main.js
@@ -1,19 +1,7 @@
 import { createApp } from 'vue'
 import router from "./utils/router"
 import pinia from "./utils/pinia"
-
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-// Components
+import vuetify from "./utils/vuetifyUtils"
 import App from './App.vue'
-
-const vuetify = createVuetify({
-  components,
-  directives,
-})
 
 createApp(App).use(router).use(vuetify).use(pinia).mount('#app')
