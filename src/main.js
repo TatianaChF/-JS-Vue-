aee14ca3bb3d7dc2603682f37153d18c7a91a2bb
TatianaChF <tatiana.chefox.gmail.com>
2024-09-27 09:33:09 +0300

    merging two vuetify files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import router from "./utils/router"
 import pinia from "./utils/pinia"
-import vuetify from "./utils/vuetifyUtils"
+import vuetify from "./utils/vuetify"
 import App from './App.vue'
 
 createApp(App).use(router).use(vuetify).use(pinia).mount('#app')
